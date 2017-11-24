7dbcbaac06922db272db3b17e5ee2fbd9d63a7c0
yangtengjiao <yangtengjiao@peilian.com>
2017-11-24 19:01:22 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# microservice
-spring-cloud微服务
+# * spring-cloud微服务
+
+## * spring-cloud-eureka-server 		【Eureka注册服务】
+
+## * spring-cloud-eureka-client 		【服务提供方】
+
+## * spring-cloud-eureka-consumenr 	【服务消费方】
+
+## * api-zuul							【路由服务网关】
+
+## * hystrix-dashboard					【服务监控】
---
+++ api-zuul/src/main/java/com/tenmue/springcloud/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.tenmue.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * zuul服务网关
+ * @author USER
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
